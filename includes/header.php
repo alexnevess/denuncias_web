1d25d2f22f7d2aefd8d74d4bd0e9f8921d83ad9e
Alexsandro Neves da Silva Filho <alexsndsf@gmail.com>
2025-06-28 16:41:45 -0300

    Cria o include header

---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/denuncias_web/styles/styleHeader.css">
+    <link rel="stylesheet" href="../styles/style.css">
+    
+</head>
+
+<body>
+    <header>
+    <a href = "../index.php">teste<!--<img src="" alt="">--></a>
+    <nav>
+        <ul>
+            <li><a href = "../views/denuncias.php">Ver denúncias</a></li>
+            <li><a class="linkDen" href="../index.php">Denuncie</a></li>
+        </ul>
+    </nav>
+    </header>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -10,18 +10,20 @@
 </head>
 
 <body>
+    <?php
+    include "includes/header.php";
+    ?>
     <main class="mainIndex">
-    <form class="formDenuncia" action="actions/salva_denuncia.php" method="POST" enctype="multipart/form-data">
-        <input type="text" name="descricao" rows="4" class="inputDenuncia">
-        <?php if(isset($_GET['erro']))
-        {
-            echo "Descrição é um item obrigatório";
-        }?>
-        <input type="file" name="imagem" class="inputDenuncia">
-        <input type="text" name="endereco" class="inputDenuncia">
-        <input type="hidden" name="confirma" value="0" class="inputDenuncia">
-        <input class="btnDenuncia" type="submit" value="Denunciar">
-    </form>
+        <form class="formDenuncia" action="actions/salva_denuncia.php" method="POST" enctype="multipart/form-data">
+            <input type="text" name="descricao" rows="4" class="inputDenuncia">
+            <?php if (isset($_GET['erro'])) {
+                echo "Descrição é um item obrigatório";
+            } ?>
+            <input type="file" name="imagem" class="inputDenuncia">
+            <input type="text" name="endereco" class="inputDenuncia">
+            <input type="hidden" name="confirma" value="0" class="inputDenuncia">
+            <input class="btnDenuncia" type="submit" value="Denunciar">
+        </form>
     </main>
 </body>
 
